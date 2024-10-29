27c0c99aa29c1d185d5e73975eaea6a785de09d3
M01010 <ihaamoodi@gmail.com>
2024-10-29 15:03:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import time
+from selenium.webdriver import Firefox, Chrome, Edge
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from scraper import download_to_dir, get_game_id, get_image_link
+
+browsers: dict[str, type[WebDriver]] = {
+    'firefox': Firefox,
+    'chrome': Chrome,
+    'edge': Edge
+}
+browser_options = list(browsers.keys())
+
+
+def main(argv: list[str], argc: int):
+    if argc != 3:
+        print("invalid args")
+        print("arg0: shortcuts directory")
+        print("arg1: steam icons directory")
+        print(f"arg2: browser type options={browser_options}")
+
+        return
+    homedir = argv[0]
+    steam_icon_dir = argv[1]
+    browser = argv[2].lower()
+    assert os.path.isdir(homedir) and os.path.isdir(steam_icon_dir), "invalid paths"
+    assert browser in browsers.keys(), f"invalid browser, options={browser_options}"
+
+
+    print("Script Started")
+    print(f"Opening {browser}")
+    with browsers[browser]() as webdriver:
+        print(f"Opened {browser}")
+        for filename in os.listdir(homedir):
+            g = get_game_id(filename, homedir)
+            if not g:
+                continue
+            print(f"Found Game({g}) in directory")
+            link = get_image_link(webdriver, g.steam_id)
+            print(f"Got link={link}")
+            if not link:
+                print(f"Error {g} has no link")
+                continue
+            download_to_dir(link, steam_icon_dir)
+            print(f"Downloaded icon\n")
+            time.sleep(2)
+        print(f"Closing {browser}")
+    print("Script Finished")
+
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    main(args, len(args))
---
+++ scraper.py
@@ -0,0 +1,69 @@
+
+import os
+import re
+from typing import Optional
+
+from pydantic import BaseModel
+import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+import bs4
+
+
+regex = re.compile("URL=steam://rungameid/[0-9]+")
+
+class GameShortcut(BaseModel):
+    name: str
+    steam_id: str
+
+def get_game_id(f: str, homedir: str) -> Optional[GameShortcut]:
+    if not f.endswith('.url'):
+        return None
+    if not os.path.isfile(os.path.join(homedir, f)):
+        return None
+    
+    with open(os.path.join(homedir, f)) as f2:
+        for l in f2.readlines():
+            if regex.match(l):
+                steam_id =  l.strip().lstrip('URL=steam://rungameid/')
+                name = f.rstrip('.url')
+                return GameShortcut(name=name, steam_id=steam_id)
+    return None
+
+
+
+def download_to_dir(url: str, dir: str):
+    r = requests.get(url)
+    fname = get_file_name(r, url)
+    imagepath = os.path.join(dir, fname)
+    with open(imagepath, "wb") as f:
+        f.write(r.content)
+    
+
+def get_file_name(r: requests.Response, url: str) -> str:
+    fname = ''
+    if "Content-Disposition" in r.headers.keys():
+        fname = re.findall("filename=(.+)", r.headers["Content-Disposition"])[0]
+    else:
+        fname = url.split("/")[-1]
+    return fname
+
+def get_steamdb_url(game_id: int):
+    return f"https://steamdb.info/app/{game_id}/info/"
+
+
+def get_image_link(webdriver: WebDriver, game_id) -> str | None:
+    webdriver.get(get_steamdb_url(game_id))
+
+    table = webdriver.find_element(by=By.ID, value="js-assets-table")
+    inner = table.get_attribute("innerHTML")
+    if not inner:
+        return None
+    soup = bs4.BeautifulSoup(inner, "html.parser")
+    tbody: bs4.Tag = soup.find("tbody") # type: ignore
+    row_list = tbody.findChildren("tr", recursive=False)
+
+    for r in row_list:
+        columns = r.findChildren("td", recursive=False)
+        if columns[0].text == "clienticon":
+            return columns[1].find("a")['href']
